0b81b090778be2721db5694e2b2219d0228e103f
SilionZ <30394571+SilionZ@users.noreply.github.com>
2018-03-18 01:01:13 +1300

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,25 @@
+/*
+ * us p5.js to draw a clock on a 960x500 canvas
+ */ 
+function draw_clock(obj) {
+    // draw your own clock here based on the values of obj:
+    //    obj.hours goes from 0-23
+    //    obj.minutes goes from 0-59
+    //    obj.seconds goes from 0-59
+    //    obj.millis goes from 0-1000
+    //    obj.seconds_until_alarm is:
+    //        < 0 if no alarm is set
+    //        = 0 if the alarm is currently going off
+    //        > 0 --> the number of seconds until alarm should go off
+}
+var img;  // Declare variable 'img'.
+
+function setup() {
+  createCanvas(720, 400);
+  img = loadImage("assets/round.png");  // Load the image
+}
+
+function draw() {
+  // Displays the image at its actual size at point (0,0)
+  image(img, 0, 0);
+}
